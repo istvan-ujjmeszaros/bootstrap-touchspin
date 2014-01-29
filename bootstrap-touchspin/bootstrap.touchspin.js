f56cb5790235715eda993f53493aef93200f6a4b
István Ujj-Mészáros <styu007@gmail.com>
2014-01-29 14:12:35 +0100

    reverting latest changes...

--- bootstrap-touchspin/bootstrap.touchspin.js
+++ bootstrap-touchspin/bootstrap.touchspin.js
@@ -3,7 +3,7 @@
 
 /*!=========================================================================
  *  Bootstrap TouchSpin
- *  v2.6.0
+ *  v2.6.1
  *
  *  A mobile and touch friendly input spinner component for Bootstrap 3.
  *
@@ -530,9 +530,6 @@
                 spincount = 0;
                 spinning = "down";
 
-                originalinput.trigger("touchspin.startspin");
-                originalinput.trigger("touchspin.startdownspin");
-
                 downDelayTimeout = setTimeout(function() {
                     downSpinTimer = setInterval(function() {
                         spincount++;
@@ -547,9 +544,6 @@
                 spincount = 0;
                 spinning = "up";
 
-                originalinput.trigger("touchspin.startspin");
-                originalinput.trigger("touchspin.startupspin");
-
                 upDelayTimeout = setTimeout(function() {
                     upSpinTimer = setInterval(function() {
                         spincount++;
@@ -563,8 +557,6 @@
                 clearTimeout(upDelayTimeout);
                 clearInterval(downSpinTimer);
                 clearInterval(upSpinTimer);
-
-                originalinput.trigger("touchspin.stopspin");
 
                 spincount = 0;
                 spinning = false;
