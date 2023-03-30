4e3f11940b359d821caa3ae1fbea8a0dc19a953a
Istvan Ujj-Meszaros <styu007@gmail.com>
2023-03-30 06:51:10 +0200

    Building dist files with grunt

--- dist/jquery.bootstrap-touchspin.js
+++ dist/jquery.bootstrap-touchspin.js
@@ -599,7 +599,7 @@
 
         returnval = _forcestepdivisibility(returnval);
 
-        if (Number(val).toString() !== returnval.toString()) {
+        if (parseFloat(Number(val).toString()) !== parseFloat(returnval.toString())) {
           originalinput.val(returnval);
           originalinput.trigger('change');
         }
