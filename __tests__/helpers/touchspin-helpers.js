9f935f43d50cfaacfee9688056a92a06b08b0095
Istvan Ujj-Meszaros <styu007@gmail.com>
2023-03-30 08:36:02 +0200

    Adding a changeEventCounter method to the test helpers

--- __tests__/helpers/touchspin-helpers.js
+++ __tests__/helpers/touchspin-helpers.js
@@ -25,4 +25,13 @@ async function touchspinClickUp(page, input_selector) {
   }, input_selector + ' + .input-group-btn > .bootstrap-touchspin-up');
 }
 
-module.exports = { touchspinClick, touchspinClickUp };
+async function changeEventCounter(page) {
+  // Get the event log content
+  const eventLogContent = await page.$eval('#events_log', el => el.textContent);
+
+  // Count the number of 'change' events
+  //const changeEventCounter = (eventLogContent.match(/change/g) || []).length;
+  return (eventLogContent.match(/change/g) || []).length;
+}
+
+module.exports = { touchspinClick, touchspinClickUp, changeEventCounter };
