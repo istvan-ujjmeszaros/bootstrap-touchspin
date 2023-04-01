c07dff4bfd229fba689d0e4f6e37c7be62257f87
Istvan Ujj-Meszaros <styu007@gmail.com>
2023-04-01 23:53:41 +0200

    Renaming the existing test file

--- __tests__/events.test.js
+++ __tests__/events.test.js
@@ -14,7 +14,7 @@ const server = app.listen(port, () => {
   console.log(`Express server listening on port ${port}...`);
 });
 
-describe('TouchSpin Tests', () => {
+describe('Events', () => {
   let browser;
   let page;
 
