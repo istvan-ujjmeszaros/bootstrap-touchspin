4b48777a58fa20cc219228f3c79b420546627159
Istvan Ujj-Meszaros <styu007@gmail.com>
2023-04-03 15:28:22 +0200

    Using the helper method to read the value

--- __tests__/basicOperations.test.ts
+++ __tests__/basicOperations.test.ts
@@ -7,36 +7,34 @@ describe('Core functionality', () => {
     const selector: string = '#testinput1';
 
     const button = await page.$(selector + ' + .input-group-btn > .bootstrap-touchspin-up');
+
     expect(button).toBeTruthy();
   });
 
   it('should increase value by 1 when clicking the + button', async () => {
+    const selector: string = '#testinput1';
+
     // We have to use the mousedown and mouseup events because the plugin is not handling the click event.
-    await touchspinHelpers.touchspinClickUp(page, '#testinput1');
+    await touchspinHelpers.touchspinClickUp(page, selector);
 
-    const input = await page.$('#testinput1');
-    const value = await input?.evaluate((el) => (el as HTMLInputElement).value);
-
-    expect(value).toBe('51');
+    expect(await touchspinHelpers.readValue(page, selector)).toBe('51');
   });
 
   it('should not increase when clicking the + button with a disabled input', async () => {
+    const selector: string = '#testinput1';
+
     // We have to use the mousedown and mouseup events because the plugin is not handling the click event.
-    await touchspinHelpers.touchspinClickUp(page, '#testinput1');
+    await touchspinHelpers.touchspinClickUp(page, selector);
 
-    const input = await page.$('#testinput1');
-    const value = await input?.evaluate((el) => (el as HTMLInputElement).value);
-
-    expect(value).toBe('51');
+    expect(await touchspinHelpers.readValue(page, selector)).toBe('51');
   });
 
   it('should not increase when clicking the + button with a readonly input', async () => {
+    const selector: string = '#testinput1';
+
     // We have to use the mousedown and mouseup events because the plugin is not handling the click event.
-    await touchspinHelpers.touchspinClickUp(page, '#testinput1');
+    await touchspinHelpers.touchspinClickUp(page, selector);
 
-    const input = await page.$('#testinput1');
-    const value = await input?.evaluate((el) => (el as HTMLInputElement).value);
-
-    expect(value).toBe('51');
+    expect(await touchspinHelpers.readValue(page, selector)).toBe('51');
   });
 });
