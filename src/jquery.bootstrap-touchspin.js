929a28ad3322e2e99c7b1479470f926a8621c03b
Istvan Ujj-Meszaros <styu007@gmail.com>
2023-05-25 06:22:58 +0200

    forcestepdivisibility is now respected

--- src/jquery.bootstrap-touchspin.js
+++ src/jquery.bootstrap-touchspin.js
@@ -155,19 +155,8 @@
         settings = $.extend({}, defaults, originalinput_data, _parseAttributes(), options);
 
         if (parseFloat(settings.step) !== 1) {
-          let remainder;
-
-          // Modify settings.max to be divisible by step
-          remainder = settings.max % settings.step;
-          if (remainder !== 0) {
-            settings.max = parseFloat(settings.max) - remainder;
-          }
-
-          // Do the same with min, should work with negative numbers too
-          remainder = settings.min % settings.step;
-          if (remainder !== 0) {
-            settings.min = parseFloat(settings.min) + (parseFloat(settings.step) - remainder);
-          }
+          settings.min = _forcestepdivisibility(settings.min);
+          settings.max = _forcestepdivisibility(settings.max);
         }
       }
 
